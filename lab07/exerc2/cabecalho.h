7d3ec7d28825efc98f0c3b62d845b6a2f3aa2c8a
liviuss04 <93675308+liviuss04@users.noreply.github.com>
2022-07-03 19:23:01 -0300

    Delete lab07 directory

--- lab07/exerc2/cabecalho.h
+++
@@ -1,8 +0,0 @@
-typedef struct no *Lista;
-
-Lista criar_lista();
-int lista_vazia(Lista lst);
-void lista_cheia(Lista lst);
-int insere_elem(Lista *lst, int elem);
-int remove_elem(Lista *lst, int elem);
-void obtem_valor_elem(Lista lst);
--- lab07/exerc4/lista.h
+++
@@ -1,8 +0,0 @@
-typedef struct no *Lista;
-
-Lista criar_lista();
-int lista_vazia(Lista lst);
-void lista_cheia(Lista lst);
-int insere_elem(Lista *lst);
-int remove_elem(Lista *lst);
-void obtem_valor_elem(Lista lst);
